9c314773d18749d3692c4362c67f4763768c4d9e
Technophile <dostonnabotov@gmail.com>
2025-05-24 11:01:58 +0300

    Fix redirect causing pages not to respond

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -3,14 +3,13 @@
 import Header from "@/components/Header";
 import StoryViewer from "@/components/StoryViewer";
 import { useAppContext } from "@/contexts/AppContext";
-// import { redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Play() {
   const { isSiteOpen } = useAppContext();
 
   if (!isSiteOpen) {
-    console.log("Site isn't open");
-    // redirect("/home");
+    redirect("/home");
   }
 
   return (
